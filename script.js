1866b2b3ab534a7430cbb0f09610c84a19ab7852
rish1o1 <101313723+rish1o1@users.noreply.github.com>
2024-02-21 00:14:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function getWeather() {
     if (!city) {
         alert('Please enter a city');
         return;
+        
     }
 
     const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
